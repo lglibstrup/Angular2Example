d8a14e095fcc1816881fa9d8c6ceacb2366a8a13
Lasse Glibstrup <lglibstrup@gmail.com>
2016-08-10 12:36:22 +0200

    Add project files.

---
+++ src/MC.Angular/app/app.module.ts
@@ -0,0 +1,43 @@
+﻿import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+
+// Imports for loading & configuring the in-memory web api
+import { HttpModule, XHRBackend } from '@angular/http';
+
+import { InMemoryBackendService, SEED_DATA } from 'angular2-in-memory-web-api';
+import { InMemoryDataService }               from './services/in-memory-data.service';
+
+import { AppComponent }   from './app.component';
+import { routing }        from './app.routing';
+
+import { HeroesComponent }      from './components/heroes.component';
+import { DashboardComponent }   from './components/dashboard.component';
+import { HeroDetailComponent }  from './components/hero-detail.component';
+import { HeroSearchComponent }  from './components/hero-search.component';
+
+import { HeroService }  from './services/hero.service';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        FormsModule,
+        routing,
+        HttpModule
+    ],
+    declarations: [
+        AppComponent,
+        HeroesComponent,
+        DashboardComponent,
+        HeroDetailComponent,
+        HeroSearchComponent
+    ],
+    providers: [
+        HeroService,
+        { provide: XHRBackend, useClass: InMemoryBackendService }, // in-mem server
+        { provide: SEED_DATA, useClass: InMemoryDataService }     // in-mem server data
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
